eaa7930acdb548e10b0b817e7910b4e8e2b5584b
luiscastro193 <luiscastro193@gmail.com>
2024-09-23 18:33:59 +0200

    Generalizable debounce

--- svg-inliner.js
+++ svg-inliner.js
@@ -4,24 +4,24 @@ async function request(resource, options) {
 	if (response.ok) return response; else throw response;
 }
 
+function debounce(callback) {
+	let pending = false;
+	return () => {
+		if (!pending) {
+			pending = true;
+			setTimeout(() => {
+				pending = false;
+				callback();
+			}, 0);
+		}
+	}
+}
+
 export default async function inline(svg) {
 	svg.removeAttribute('data-inline');
 	svg.outerHTML = await (await request(svg.src)).text();
 }
 
-let pending = false;
-
-function inject() {
-	pending = false;
-	document.body.querySelectorAll('img[data-inline]').forEach(inline);
-}
-
-function requestInject() {
-	if (!pending) {
-		pending = true;
-		setTimeout(inject, 0);
-	}
-}
-
-new MutationObserver(requestInject).observe(document, {subtree: true, childList: true});
-requestInject();
+const inject = debounce(() => document.body.querySelectorAll('img[data-inline]').forEach(inline));
+new MutationObserver(inject).observe(document, {subtree: true, childList: true});
+inject();
